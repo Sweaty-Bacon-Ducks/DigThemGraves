0510cee72dcf7d9f22a225c1e5d2d2526b5ed795
KowalikJakub <kowalik.kub@gmail.com>
2019-07-07 09:46:04 +0200

    Empty grave interfaces and test scene

---
+++ Assets/Scripts/Grave.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace DigThemGraves
+{
+    [CreateAssetMenu(fileName = "Grave", menuName = "In-Game-Objects")]
+    public class Grave : ScriptableObject
+    {
+
+    }
+}
