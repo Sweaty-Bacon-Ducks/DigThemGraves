274a1a995dd20b7116b68abd65d0d7e8340d8d13
KowalikJakub <kowalik.kub@gmail.com>
2019-07-14 15:30:21 +0200

    Added simple movement and screen pinching

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerContoller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rb;
+    [SerializeField]
+    private string horizontalAxisName;
+    [SerializeField]
+    private string verticalAxisName;
+    [SerializeField]
+    private float acceleration;
+
+    public float Acceleration { get => acceleration; set => acceleration = value; }
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        var horizontalInput = Input.GetAxis(horizontalAxisName);
+        var verticalInput = Input.GetAxis(verticalAxisName);
+        var inputVector = new Vector2(horizontalInput, verticalInput).normalized;
+
+        if (inputVector.magnitude > 0)
+        {
+            rb.AddForce(acceleration * (new Vector2(horizontalInput, verticalInput).normalized));
+        }
+        else
+        {
+            if (rb.velocity.magnitude >= 0.05f)
+            {
+                rb.AddForce(-acceleration * rb.velocity.normalized);
+            }
+            else
+            {
+                rb.velocity = Vector2.zero;
+            }
+        }
+
     }
 }
---
+++ Assets/Scripts/ZoomableCamera.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using UnityEngine;
+
+namespace DigThemGraves
+{
+    public abstract class InputZoom
+    {
+        public abstract float Sensitivity { get; }
+        public abstract float ZoomValue { get; }
+
+        public static InputZoom Create(float Sensitivity)
+        {
+#if UNITY_ANDROID
+            return new PinchZoom(Sensitivity);
+#endif
+#if UNITY_IOS
+            throw new PlatformNotSupportedException("Current build target: IOS");
+#else
+            return new MouseWheelZoom(Sensitivity);
+#endif
+        }
+    }
+
+    public class MouseWheelZoom : InputZoom
+    {
+        private float sensitivity;
+
+        public MouseWheelZoom(float Sensitivity)
+        {
+            this.sensitivity = Sensitivity;
+        }
+        public override float Sensitivity => sensitivity;
+        public override float ZoomValue
+        {
+            get
+            {
+                return sensitivity * Input.mouseScrollDelta.y;
+            }
+        }
+    }
+
+    [Serializable]
+    public class PinchZoom : InputZoom
+    { 
+        private float sensitivity;
+
+        public PinchZoom(float Sensitivity)
+        {
+            this.sensitivity = Sensitivity;
+        }
+
+        public override float Sensitivity => sensitivity;
+
+        public override float ZoomValue
+        {
+            get
+            {
+                var pinchAmmount = 0.0f;
+                if (Input.touchCount == 2)
+                {
+                    var firstTouch = Input.GetTouch(0);
+                    var secondTouch = Input.GetTouch(1);
+
+                    var firstTouchesCurrentPosition = firstTouch.position;
+                    var secondTouchesCurrentPosition = secondTouch.position;
+
+                    var firstTouchesOldPosition = firstTouch.position - firstTouch.deltaPosition;
+                    var secondTouchesOldPosition = secondTouch.position - secondTouch.deltaPosition;
+
+                    var oldMagnitude = (firstTouchesOldPosition - secondTouchesOldPosition).magnitude;
+                    var currentMagnitude = (firstTouchesCurrentPosition - secondTouchesCurrentPosition).magnitude;
+
+                    pinchAmmount = currentMagnitude - oldMagnitude;
+
+                    return sensitivity * pinchAmmount;
+                }
+                return pinchAmmount;
+            }
+        }
+    }
+
+    [RequireComponent(typeof(Camera))]
+    public class ZoomableCamera : MonoBehaviour
+    {
+        private Camera targetCamera;
+        private InputZoom zoomGesture;
+
+        [SerializeField]
+        private float minZoom;
+        [SerializeField]
+        private float maxZoom;
+        [SerializeField]
+        private float sensitivity;
+
+        public float MinZoom => minZoom;
+        public float MaxZoom => maxZoom;
+        public float Sensitivity => sensitivity;
+
+        private void Awake()
+        {
+            zoomGesture = InputZoom.Create(sensitivity);
+            targetCamera = GetComponent<Camera>();
+        }
+
+        private void Update()
+        {
+            var currentSize = targetCamera.orthographicSize;
+            targetCamera.orthographicSize = Mathf.Clamp(currentSize - zoomGesture.ZoomValue,
+                                                        MinZoom,
+                                                        MaxZoom);
+        }
+    }
+}
