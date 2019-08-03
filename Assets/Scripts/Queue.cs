0749edda16ae6ab2d57237cf0328774ad2343a03
Kacper Obara <obara.kacper0@gmail.com>
2019-08-03 23:35:23 +0200

    Initial commit (#10)

---
+++ Assets/Scripts/Queue.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace DigThemGraves
+{
+    public abstract class Queue : MonoBehaviour
+    {
+        public abstract void AddAction(IAction action);
+        public abstract void RemoveAction(IAction action);
+    }
+}
